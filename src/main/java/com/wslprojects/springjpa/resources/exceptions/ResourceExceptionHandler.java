e1cb87b05e14e1b3a1c3625c720b2177865732db
WagnerSousaLima <wagner.sousalima@gmail.com>
2024-07-14 20:00:09 -0300

    Exception handling - delete

--- src/main/java/com/wslprojects/springjpa/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/wslprojects/springjpa/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.wslprojects.springjpa.resources.exceptions;
 
+import com.wslprojects.springjpa.services.exceptions.DatabaseException;
 import com.wslprojects.springjpa.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.zip.DataFormatException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -17,6 +19,17 @@ public class ResourceExceptionHandler {
     resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resouce not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandartError err = new StandartError
+                (Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+    @ExceptionHandler(DataFormatException.class)
+    public ResponseEntity<StandartError>
+    database(DatabaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandartError err = new StandartError
                 (Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
